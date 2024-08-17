9f843b72b38c26583aa23d9bcec40d1c4e7c75dd
Mateo21cardozo <mateo21cardozo@gmail.com>
2024-08-17 00:10:49 -0300

    fix: load images

--- docs/chunk-DJGLKOGL.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./chunk-7MNRRA2V.js";import"./chunk-H4CQMXA5.js";import"./chunk-VNTZ2B4D.js";export{a as PortfolioComponent};
--- docs/chunk-YBOY2CK4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-KINPGKOR.js";import"./chunk-H4CQMXA5.js";import"./chunk-VNTZ2B4D.js";export{a as AboutComponent};
